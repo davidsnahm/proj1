423a19f00ea91c04144db5ac0bd99e958bd35f10
David Nahm <David@airbears2-10-142-71-118.airbears2.1918.berkeley.edu>
2016-03-17 23:34:19 -0700

    finished proj1

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -9,12 +9,35 @@ class PokemonsController < ApplicationController
 	end
 
 	def damage
-		puts params
 		@pokemon = Pokemon.find(params[:id])
+		puts @pokemon.health
 		@pokemon.health -= 10
+		@pokemon.save
 		if @pokemon.health <= 0
 			Pokemon.find(params[:id]).destroy
 		end
-		redirect_to action: 'trainers#show'
+		redirect_to controller: 'trainers', action: 'show', id: @pokemon.trainer_id
+	end
+
+	def new 
+		@pokemon = Pokemon.new
+	end
+
+	def create 
+		@pokemon = Pokemon.create user_params
+		@pokemon.trainer_id = current_trainer.id
+		@pokemon.health = 100
+		@pokemon.level = 1
+		if @pokemon.save
+			redirect_to controller: 'trainers', action: 'show', id: @pokemon.trainer_id
+		else 
+			redirect_to '/pokemons/new'
+			flash[:error] = @pokemon.errors.full_messages.to_sentence
+		end
+	end
+
+	private
+	def user_params
+		params.require(:pokemon).permit(:name)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :trainers
   patch 'capture', to: 'pokemons#capture', as: 'capture'
   patch 'damage', to: 'pokemons#damage', as: 'damage'
+  post '/pokemons', to: 'pokemons#create'
+  get '/pokemons/new', to: 'pokemons#new'
 end
